d189e01cc35794881faf269ad67f453bb6ddc4fe
bryan vera <bveracachay@gmail.com>
2024-03-11 13:08:00 -0500

    meta data

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,8 @@
 import Form from "./components/Form";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Baby Shawer de Vasco</title>
-        <meta name="description" content="Regalo" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className="flex min-h-screen w-full items-center justify-center  bg-[#b4e4ff]">
         <Form />
       </main>
